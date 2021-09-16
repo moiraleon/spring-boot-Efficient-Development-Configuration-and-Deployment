6528736976dfd29fa98fb4fdf3ede032faa64db2
Moira Leon <moira.leon2@gmail.com>
2021-09-16 16:38:27 -0600

    initial commit

---
+++ src/test/java/schultz/dustin/io/justgifit/JustGifItApplicationTests.java
@@ -0,0 +1,13 @@
+package schultz.dustin.io.justgifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustGifItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
