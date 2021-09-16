5a3d81107ada5a4ab9f5b1ef30c5ab9f7a2bdef2
Moira Leon <moira.leon2@gmail.com>
2021-09-16 17:15:18 -0600

    Overriding App Properties at Runtime

--- src/main/java/schultz/dustin/io/justgifit/controller/UploadController.java
+++ src/main/java/schultz/dustin/io/justgifit/controller/UploadController.java
@@ -1,18 +1,20 @@
 package schultz.dustin.io.justgifit.controller;
 
+import java.io.File;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.logging.Logger;
 
 @RestController
 public class UploadController {
 
-    private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup()
+    private final static Logger log = (Logger) LoggerFactory.getLogger(MethodHandles.lookup()
             .lookupClass());
 
 
@@ -25,7 +27,12 @@ public class UploadController {
                          @RequestParam("start") int start,
                          @RequestParam("end") int end,
                          @RequestParam("speed") int speed,
-                         @RequestParam("repeat") boolean repeat){
+                         @RequestParam("repeat") boolean repeat) throws IOException {
+        File videoFile = new File(location + "/" + System
+                .currentTimeMillis()+".mp4");
+        file.transferTo(videoFile);
+        log.info("Saved file to {}", videoFile.getAbsolutePath());
+        return "";
 
     }
 }
